22ddfca396c7b84156875810bdd91734a8a4b9fe
alostsock <49344439+alostsock@users.noreply.github.com>
2020-12-11 19:44:43 -0500

    Improve readability (?) for day 11

--- lib/2020/202011.ex
+++ lib/2020/202011.ex
@@ -18,7 +18,6 @@ defmodule AOC202011 do
     Map.values(map) |> count_occupied_seats()
   end
 
-
   defp seatmap do
     raw = AOC.input(__MODULE__) |> Enum.map(&String.graphemes/1)
     width = length(raw)
@@ -51,15 +50,38 @@ defmodule AOC202011 do
         for x <- 0..(width - 1),
             y <- 0..(height - 1),
             into: %{},
-            do:
-              {{x, y},
-               case strategy do
-                 :part1 -> change_state1({x, y}, map)
-                 :part2 -> change_state2({x, y}, map)
-               end}
+            do: {{x, y}, update_seat({x, y}, map, strategy)}
 
       populate({new_map, width, height}, strategy)
     end
+  end
+
+  defp update_seat(pos, seatmap, strategy) do
+    {state, _} = seatmap[pos]
+
+    {count, n} =
+      case strategy do
+        :part1 -> {&count_adjacent/2, 4}
+        :part2 -> {&count_linear/2, 5}
+      end
+
+    cond do
+      state == :empty and count.(pos, seatmap) == 0 -> {:occupied, :fresh}
+      state == :occupied and count.(pos, seatmap) >= n -> {:empty, :fresh}
+      true -> {state, :stale}
+    end
+  end
+
+  defp count_adjacent(pos, seatmap) do
+    adjacent_coords(pos)
+    |> Enum.map(&seatmap[&1])
+    |> count_occupied_seats()
+  end
+
+  defp count_linear(pos, seatmap) do
+    directions()
+    |> Enum.map(&look(pos, seatmap, &1))
+    |> count_occupied_seats()
   end
 
   defp count_occupied_seats(seats) do
@@ -72,55 +94,26 @@ defmodule AOC202011 do
   end
 
   defp adjacent_coords({x, y}) do
-    coords = for u <- (x - 1)..(x + 1), v <- (y - 1)..(y + 1), do: {u, v}
-    List.delete(coords, {x, y})
-  end
-
-  defp count_adjacent_occupied(pos, seatmap) do
-    adjacent_coords(pos)
-    |> Enum.map(&(seatmap[&1]))
-    |> count_occupied_seats()
-  end
-
-  defp change_state1(pos, seatmap) do
-    {state, _} = seatmap[pos]
-    occupied = count_adjacent_occupied(pos, seatmap)
-
-    cond do
-      state == :empty and occupied == 0 -> {:occupied, :fresh}
-      state == :occupied and occupied >= 4 -> {:empty, :fresh}
-      true -> {state, :stale}
-    end
+    for u <- (x - 1)..(x + 1),
+        v <- (y - 1)..(y + 1),
+        {u, v} != {x, y},
+        do: {u, v}
   end
 
   defp directions() do
-    directions = for u <- -1..1, v <- -1..1, do: {u, v}
-    List.delete(directions, {0, 0})
+    for u <- -1..1,
+        v <- -1..1,
+        {u, v} != {0, 0},
+        do: {u, v}
   end
 
   defp look({x, y}, seatmap, {u, v} = dir) do
     next_pos = {x + u, y + v}
     next_seat = seatmap[next_pos]
+
     case next_seat do
       {:floor, _} -> look(next_pos, seatmap, dir)
       _ -> next_seat
-    end
-  end
-
-  defp count_linear_occupied(pos, seatmap) do
-    directions()
-    |> Enum.map(&look(pos, seatmap, &1))
-    |> count_occupied_seats()
-  end
-
-  defp change_state2(pos, seatmap) do
-    {state, _} = seatmap[pos]
-    count = count_linear_occupied(pos, seatmap)
-
-    cond do
-      state == :empty and count == 0 -> {:occupied, :fresh}
-      state == :occupied and count >= 5 -> {:empty, :fresh}
-      true -> {state, :stale}
     end
   end
 end
