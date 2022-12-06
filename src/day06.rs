6b4050a2077700b0159aa76123a645e5e2a3a507
alostsock <hi@alostsock.dev>
2022-12-06 02:06:17 -0500

    implement faster version of day 6, ~10x speedup

--- src/day06.rs
+++ src/day06.rs
@@ -1,5 +1,5 @@
 use crate::Solution;
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 #[derive(Default)]
 pub struct Day6 {}
@@ -9,15 +9,16 @@ impl Solution for Day6 {
     type P2 = usize;
 
     fn part_1(&self) -> Self::P1 {
-        first_marker(include_str!("data/day06"), 4)
+        find_fast(include_str!("data/day06"), 4)
     }
 
     fn part_2(&self) -> Self::P2 {
-        first_marker(include_str!("data/day06"), 14)
+        find_fast(include_str!("data/day06"), 14)
     }
 }
 
-fn first_marker(s: &str, length: usize) -> usize {
+#[allow(dead_code)]
+fn find(s: &str, length: usize) -> usize {
     let slice: Vec<_> = s.chars().collect();
     let index = slice
         .windows(length)
@@ -26,25 +27,88 @@ fn first_marker(s: &str, length: usize) -> usize {
     index + length
 }
 
+fn find_fast(s: &str, length: usize) -> usize {
+    let mut unique_char_count = 0;
+    let mut seen = HashMap::<char, usize>::new();
+
+    // initialize `seen`
+    for char in s.get(0..length - 1).unwrap().chars() {
+        seen.entry(char)
+            .and_modify(|count| {
+                *count += 1;
+                if *count == 1 {
+                    unique_char_count += 1;
+                }
+            })
+            .or_insert_with(|| {
+                unique_char_count += 1;
+                1
+            });
+    }
+
+    // iterate through the rest
+    let slice: Vec<_> = s.chars().collect();
+    for (i, window) in slice.windows(length).enumerate() {
+        // add last char
+        seen.entry(*window.last().unwrap())
+            .and_modify(|count| {
+                *count += 1;
+                if *count == 1 {
+                    unique_char_count += 1;
+                }
+            })
+            .or_insert_with(|| {
+                unique_char_count += 1;
+                1
+            });
+
+        if unique_char_count == length {
+            return i + length;
+        }
+
+        // remove first char
+        seen.entry(*window.first().unwrap()).and_modify(|count| {
+            *count -= 1;
+            if *count == 0 {
+                unique_char_count -= 1;
+            }
+        });
+    }
+
+    0
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn first_marker_works() {
-        assert_eq!(first_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 4), 7);
-        assert_eq!(first_marker("bvwbjplbgvbhsrlpgdmjqwftvncz", 4), 5);
-        assert_eq!(first_marker("nppdvjthqldpwncqszvftbrmjlhg", 4), 6);
-        assert_eq!(first_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4), 10);
-        assert_eq!(first_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4), 11);
+    fn find_works() {
+        assert_eq!(find("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 4), 7);
+        assert_eq!(find("bvwbjplbgvbhsrlpgdmjqwftvncz", 4), 5);
+        assert_eq!(find("nppdvjthqldpwncqszvftbrmjlhg", 4), 6);
+        assert_eq!(find("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4), 10);
+        assert_eq!(find("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4), 11);
+
+        assert_eq!(find("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14), 19);
+        assert_eq!(find("bvwbjplbgvbhsrlpgdmjqwftvncz", 14), 23);
+        assert_eq!(find("nppdvjthqldpwncqszvftbrmjlhg", 14), 23);
+        assert_eq!(find("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14), 29);
+        assert_eq!(find("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 14), 26);
     }
 
     #[test]
-    fn first_marker_long_works() {
-        assert_eq!(first_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14), 19);
-        assert_eq!(first_marker("bvwbjplbgvbhsrlpgdmjqwftvncz", 14), 23);
-        assert_eq!(first_marker("nppdvjthqldpwncqszvftbrmjlhg", 14), 23);
-        assert_eq!(first_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14), 29);
-        assert_eq!(first_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 14), 26);
+    fn find_fast_works() {
+        assert_eq!(find_fast("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 4), 7);
+        assert_eq!(find_fast("bvwbjplbgvbhsrlpgdmjqwftvncz", 4), 5);
+        assert_eq!(find_fast("nppdvjthqldpwncqszvftbrmjlhg", 4), 6);
+        assert_eq!(find_fast("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4), 10);
+        assert_eq!(find_fast("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4), 11);
+
+        assert_eq!(find_fast("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14), 19);
+        assert_eq!(find_fast("bvwbjplbgvbhsrlpgdmjqwftvncz", 14), 23);
+        assert_eq!(find_fast("nppdvjthqldpwncqszvftbrmjlhg", 14), 23);
+        assert_eq!(find_fast("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14), 29);
+        assert_eq!(find_fast("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 14), 26);
     }
 }
