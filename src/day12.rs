e3eb39736e7b4068bfa00b74fcf2f557b1993cb5
alostsock <hi@alostsock.dev>
2022-12-12 03:06:57 -0500

    refactor day 12 to use bfs

--- src/day12.rs
+++ src/day12.rs
@@ -1,5 +1,6 @@
+#![allow(const_item_mutation)]
 use crate::Solution;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet, VecDeque};
 
 #[derive(Default)]
 pub struct Day12 {}
@@ -9,7 +10,7 @@ impl Solution for Day12 {
 
     fn part_1(&self) -> Self::Result {
         let (grid, start, end) = parse_grid(include_str!("data/day12"));
-        find_path(&grid, start, end, 0, &mut HashMap::new()).unwrap()
+        find_path(&grid, start, end).unwrap()
     }
 
     fn part_2(&self) -> Self::Result {
@@ -51,70 +52,49 @@ fn parse_grid(input: &str) -> (Grid, P, P) {
     (grid, start, end)
 }
 
-fn find_path(
-    grid: &Grid,
-    current: P,
-    goal: P,
-    steps: usize,
-    visited: &mut HashMap<P, usize>,
-) -> Option<usize> {
-    if current == goal {
-        return Some(steps);
-    }
+fn find_path(grid: &Grid, start: P, end: P) -> Option<usize> {
+    let mut visited: HashSet<P> = HashSet::new();
+    visited.insert(start);
+    let mut to_visit: VecDeque<(P, usize)> = VecDeque::new();
+    to_visit.push_back((start, 0));
 
-    // don't continue if the current point was already
-    // visited with the same or lower step count
-    if let Some(visited_steps) = visited.get(&current) {
-        if &steps >= visited_steps {
-            return None;
+    let (r, c) = (grid.len() - 1, grid[0].len() - 1);
+
+    while let Some((current, step)) = to_visit.pop_front() {
+        if current == end {
+            return Some(step);
+        }
+
+        let (row, col) = current;
+        let height = grid[row][col];
+
+        // up
+        for next in [
+            if row > 0 { Some((row - 1, col)) } else { None },
+            if row < r { Some((row + 1, col)) } else { None },
+            if col > 0 { Some((row, col - 1)) } else { None },
+            if col < c { Some((row, col + 1)) } else { None },
+        ]
+        .iter()
+        .flatten()
+        {
+            if visited.contains(next) {
+                continue;
+            }
+
+            let next_height = grid[next.0][next.1];
+
+            if next_height < height || next_height - height <= 1 {
+                visited.insert(*next);
+                to_visit.push_back((*next, step + 1));
+            }
         }
     }
-    visited.insert(current, steps);
 
-    let (row, col) = current;
-    let (max_row, max_col) = (grid.len(), grid[0].len());
-
-    let height = grid[row][col];
-
-    let mut available_paths: Vec<P> = vec![];
-
-    let climbable = |to: usize| -> bool { to < height || to - height <= 1 };
-
-    // up
-    if row > 0 && climbable(grid[row - 1][col]) {
-        available_paths.push((row - 1, col));
-    }
-    // down
-    if row + 1 < max_row && climbable(grid[row + 1][col]) {
-        available_paths.push((row + 1, col));
-    }
-    // left
-    if col > 0 && climbable(grid[row][col - 1]) {
-        available_paths.push((row, col - 1));
-    }
-    // right
-    if col + 1 < max_col && climbable(grid[row][col + 1]) {
-        available_paths.push((row, col + 1));
-    }
-
-    if available_paths.is_empty() {
-        return None;
-    }
-
-    let mut steps_to_end: Vec<usize> = available_paths
-        .iter()
-        .filter_map(|next| find_path(grid, *next, goal, steps + 1, visited))
-        .collect();
-
-    if steps_to_end.is_empty() {
-        None
-    } else {
-        steps_to_end.sort_unstable();
-        Some(*steps_to_end.first().unwrap())
-    }
+    None
 }
 
-fn find_good_starting_point(grid: &Grid, goal: P) -> usize {
+fn find_good_starting_point(grid: &Grid, end: P) -> usize {
     let start_height = 'a' as usize;
     let mut starting_points: Vec<P> = vec![];
 
@@ -128,7 +108,7 @@ fn find_good_starting_point(grid: &Grid, goal: P) -> usize {
 
     let mut path_steps: Vec<usize> = starting_points
         .iter()
-        .filter_map(|start| find_path(grid, *start, goal, 0, &mut HashMap::new()))
+        .filter_map(|start| find_path(grid, *start, end))
         .collect();
 
     path_steps.sort_unstable();
@@ -150,9 +130,8 @@ abdefghi
     #[test]
     fn find_path_works() {
         let (grid, start, end) = parse_grid(TEST_INPUT);
-        let mut visited = HashMap::new();
-        let least_steps = find_path(&grid, start, end, 0, &mut visited);
-        assert_eq!(least_steps, Some(31));
+        let least_steps = find_path(&grid, start, end).unwrap();
+        assert_eq!(least_steps, 31);
     }
 
     #[test]
@@ -162,16 +141,13 @@ abdefghi
         assert_eq!(least_steps, 29);
     }
 
-    // needs fixing -- in an unoptimized build,
-    // both part 1 and part 2 result in stack overflows
+    #[test]
+    fn part_1_works() {
+        assert_eq!(Day12::new().part_1(), 449);
+    }
 
-    // #[test]
-    // fn part_1_works() {
-    //     assert_eq!(Day12::new().part_1(), 449);
-    // }
-
-    // #[test]
-    // fn part_2_works() {
-    //     assert_eq!(Day12::new().part_2(), 443);
-    // }
+    #[test]
+    fn part_2_works() {
+        assert_eq!(Day12::new().part_2(), 443);
+    }
 }
