2e29a6796c4bf5053e69d4af044e0497995f90cd
alostsock <hi@alostsock.dev>
2023-12-13 12:54:20 -0500

    rewrite day 12, add part 2 test

--- src/day12.rs
+++ src/day12.rs
@@ -8,11 +8,12 @@ impl Solution for Day12 {
 
     fn part_1(&self) -> Self::Result {
         let input = include_str!("data/day12");
-        count_arrangements(input)
+        count_arrangements(input, 1)
     }
 
     fn part_2(&self) -> Self::Result {
-        2023 * 25
+        let input = include_str!("data/day12");
+        count_arrangements(input, 5)
     }
 }
 
@@ -28,12 +29,12 @@ type Arrangement = Vec<Condition>;
 struct ConditionRecord {
     pattern: Arrangement,
     groups: Vec<usize>,
-    min_damaged_count: usize,
 }
 
 impl ConditionRecord {
-    fn from_str(s: &str) -> Self {
+    fn from_str(s: &str, copies: usize) -> Self {
         let (condition_str, groups_str) = s.split_once(' ').unwrap();
+
         let pattern: Arrangement = condition_str
             .chars()
             .map(|ch| match ch {
@@ -43,99 +44,87 @@ impl ConditionRecord {
                 _ => panic!("invalid condition: {ch}"),
             })
             .collect();
+        let mut pattern_copied = pattern.clone();
+        (1..copies).for_each(|_| {
+            pattern_copied.push(Condition::Unknown);
+            pattern_copied.extend(pattern.iter());
+        });
 
         let groups: Vec<usize> = groups_str
             .split(',')
             .map(|ch| ch.parse().unwrap())
             .collect();
+        let groups_copied = groups.repeat(copies);
 
         Self {
-            pattern,
-            min_damaged_count: groups.iter().sum(),
-            groups,
+            pattern: pattern_copied,
+            groups: groups_copied,
         }
     }
 
-    fn count_possible_arrangements(&self) -> usize {
-        let mut arrangement_generator = ArrangementGenerator::from_pattern(&self.pattern);
-        arrangement_generator
-            .arrangements()
-            .filter(|a| self.is_valid_arrangement(a))
-            .count()
-    }
+    fn count_arrangements(
+        &self,
+        condition_index: usize,
+        group_index: usize,
+        damaged_count: usize,
+    ) -> usize {
+        let group_count = *self.groups.get(group_index).unwrap_or(&0);
 
-    fn is_valid_arrangement(&self, arrangement: &Arrangement) -> bool {
-        if arrangement
-            .iter()
-            .filter(|&&c| c == Condition::Damaged)
-            .count()
-            < self.min_damaged_count
-        {
-            return false;
+        let damaged_conditions_needed = (group_index..self.groups.len())
+            .map(|i| *self.groups.get(i).unwrap_or(&0))
+            .sum::<usize>()
+            .saturating_sub(damaged_count);
+        let conditions_remaining = self.pattern.len() - condition_index;
+        if damaged_count > group_count || damaged_conditions_needed > conditions_remaining {
+            return 0;
         }
 
-        let damaged_groups: Vec<_> = arrangement
-            .split(|&condition| condition == Condition::Operational)
-            .filter_map(|group| {
-                if group.len() > 0 {
-                    Some(group.len())
-                } else {
-                    None
-                }
-            })
-            .collect();
+        if condition_index >= self.pattern.len() {
+            return 1;
+        }
 
-        damaged_groups == self.groups
-    }
-}
+        let current_condition = self.pattern[condition_index];
 
-struct ArrangementGenerator<'a> {
-    pattern: &'a Arrangement,
-    possibilities: usize,
-}
+        let handle_damaged = || {
+            if group_index >= self.groups.len()
+                || (damaged_count > 0 && damaged_count > group_count)
+            {
+                return 0;
+            }
 
-impl<'a> ArrangementGenerator<'a> {
-    fn from_pattern(arrangement: &'a Arrangement) -> Self {
-        let unknowns_count = arrangement
-            .iter()
-            .filter(|&&a| a == Condition::Unknown)
-            .count();
+            self.count_arrangements(condition_index + 1, group_index, damaged_count + 1)
+        };
 
-        Self {
-            pattern: arrangement,
-            possibilities: 2_usize.pow(unknowns_count.try_into().unwrap()),
+        let handle_operational = || {
+            if damaged_count > 0 && damaged_count != group_count {
+                return 0;
+            }
+
+            let group_index = if damaged_count > 0 {
+                group_index + 1
+            } else {
+                group_index
+            };
+
+            self.count_arrangements(condition_index + 1, group_index, 0)
+        };
+
+        match current_condition {
+            Condition::Damaged => handle_damaged(),
+            Condition::Operational => handle_operational(),
+            Condition::Unknown => handle_damaged() + handle_operational(),
         }
     }
-
-    fn arrangements(&mut self) -> impl Iterator<Item = Arrangement> + '_ {
-        (0..self.possibilities).map(|damaged_records_bit_mask| {
-            let mut bit_mask_offset = 0;
-
-            self.pattern
-                .iter()
-                .map(|&condition| match condition {
-                    Condition::Unknown => {
-                        let is_damaged = (damaged_records_bit_mask & (1 << bit_mask_offset)) > 0;
-
-                        bit_mask_offset += 1;
-
-                        if is_damaged {
-                            Condition::Damaged
-                        } else {
-                            Condition::Operational
-                        }
-                    }
-                    _ => condition,
-                })
-                .collect::<Arrangement>()
-        })
-    }
 }
 
-fn count_arrangements(input: &str) -> usize {
+fn count_arrangements(input: &str, copies: usize) -> usize {
     input
         .lines()
-        .map(|line| ConditionRecord::from_str(line).count_possible_arrangements())
+        .enumerate()
+        .map(|(index, line)| {
+            dbg!(index);
+            ConditionRecord::from_str(line, copies).count_arrangements(0, 0, 0)
+        })
         .sum()
 }
 
@@ -143,16 +132,21 @@ fn count_arrangements(input: &str) -> usize {
 mod tests {
     use super::*;
 
-    #[test]
-    fn part_1() {
-        let input = "???.### 1,1,3
+    const INPUT: &str = "???.### 1,1,3
 .??..??...?##. 1,1,3
 ?#?#?#?#?#?#?#? 1,3,1,6
 ????.#...#... 4,1,1
 ????.######..#####. 1,6,5
 ?###???????? 3,2,1";
 
-        assert_eq!(count_arrangements(input), 21);
-        // assert_eq!(Day12::new().part_1(), 7118);
+    #[test]
+    fn part_1() {
+        assert_eq!(count_arrangements(INPUT, 1), 21);
+        assert_eq!(Day12::new().part_1(), 7118);
+    }
+
+    #[test]
+    fn part_2() {
+        assert_eq!(count_arrangements(INPUT, 5), 525152);
     }
 }
